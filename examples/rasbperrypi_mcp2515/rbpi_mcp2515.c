e75275016b3038b49f8447e7ed438b824dd370d3
Jordan <jnod117@gmail.com>
2015-11-10 19:39:08 -0600

    Update method names

--- examples/rasbperrypi_mcp2515/rbpi_mcp2515.c
+++ examples/rasbperrypi_mcp2515/rbpi_mcp2515.c
@@ -2,7 +2,9 @@
 #include <bcm2835.h>
 #include "mcp2515.h"
 
-void spi_transfer_mcp2515(uint8_t *buf, uint8_t len) {
+char run = 1;
+
+void mcp2515_spiTransfer(uint8_t *buf, uint8_t len) {
   bcm2835_spi_transfern(buf, len); // will select device during transfer
 }
 
@@ -15,12 +17,12 @@ int main()
   bcm2835_spi_begin();
   bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
 
-  config_interrupts(0x03); // Inturrupt when a message is received
-  config_timing(CNF1_10MHZ_125KBIT, CNF2_10MHZ_125KBIT, CNF3_10MHZ_125KBIT);
-  config_receive_ctrl(0x60, 0x60); // Ignore filters, receive all messages
-  set_mode(MODE_LOOPBACK); // Loopback sends messages to itself for testing
+  mcp2515_setCANINTE(0x03); // Inturrupt when a message is received
+  mcp2515_setCNFn(CNF1_10MHZ_125KBIT, CNF2_10MHZ_125KBIT, CNF3_10MHZ_125KBIT);
+  mcp2515_setRXBnCTRL(0x60, 0x60); // Ignore filters, receive all messages
+  mcp2515_setMode(MODE_LOOPBACK); // Loopback sends messages to itself for testing
 
-  while(true) {
+  while(run) {
     if (!digitalRead(9)) {
       Serial.println("Interrupt");
     }
