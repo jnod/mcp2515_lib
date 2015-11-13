970debc7537bf99c1013d39ba85c831a950c481b
Jordan <jnod117@gmail.com>
2015-11-13 11:05:04 -0600

    Add license

--- mcp2515.c
+++ mcp2515.c
@@ -1,3 +1,19 @@
+/*
+  Copyright 2015 Jordan Nodland
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 #include "mcp2515.h"
 
 static void mcp2515_configRX(uint8_t addr,uint16_t sid,uint32_t eid,char exide);
