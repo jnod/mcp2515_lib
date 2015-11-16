3576151dfed61a6366d4e79d15b15d244c9d7a63
Jordan <jnod117@gmail.com>
2015-11-16 11:32:39 -0600

    Fix bugs

--- examples/arduino_mcp2515/arduino_mcp2515.ino
+++ examples/arduino_mcp2515/arduino_mcp2515.ino
@@ -1,40 +1,79 @@
 #include <SPI.h>
 #include <mcp2515.h>
 
-static CanMessage can_message;
+#define INT   9
+#define SS    10
+#define MOSI  11
+#define MISO  12
+#define SCK   13
+
+static CanMessage message = {MTYPE_EXTENDED_DATA, 5, 6, 2, {1,2}};
 
 void mcp2515_spiTransfer(uint8_t *buf, uint8_t len) {
-  // TODO: Select the MCP2515 if SPI.transfer(buf,len) doesn't
+  digitalWrite(SS, LOW);
   SPI.transfer(buf, len);
-  // TODO: Deselect the MCP2515 if SPI.transfer(buf,len) doesn't
+  digitalWrite(SS, HIGH);
 }
 
-void print_can_message(CanMessage message) {
-  Serial.println(message.mtype, HEX);
-  Serial.println(message.sid, DEC);
-  Serial.println(message.eid, DEC);
-  Serial.println(message.length, DEC);
-  // TODO: Print data
+void printCanMessage() {
+  Serial.print("{ Type: ");
+  Serial.print(message.mtype, DEC);
+  Serial.print(", sid: ");
+  Serial.print(message.sid, DEC);
+  Serial.print(", eid: ");
+  Serial.print(message.eid, DEC);
+  Serial.print(", length: ");
+  Serial.print(message.length, DEC);
+  Serial.print(", data: [");
+
+  uint8_t i = 0;
+  while (i < message.length) {
+    Serial.print(message.data[i], DEC);
+    i++;
+
+    if (i < message.length) Serial.print(", ");
+  }
+
+  Serial.println("] }");
 }
 
 void setup() {
-  can_message = {MTYPE_EXTENDED_DATA, 5, 6, 2, {1,2}};
-  pinMode(9, INPUT); // Used as the interrupt (INT) pin
+  pinMode(INT, INPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(MISO, INPUT);
+  pinMode(SCK, OUTPUT);
+  pinMode(SS, OUTPUT);
+  digitalWrite(SS, HIGH);
+  SPI.begin();
 
   Serial.begin(115200);
   Serial.println("Start Serial");
 
-  delay(50); // Allow the MCP2515 time to start. May not be necessary.
+  delay(100); // Allows the mcp2515 to initialize
 
+  mcp2515_reset();
   mcp2515_configCNFn(CNF1_10MHZ_125KBIT, CNF2_10MHZ_125KBIT, CNF3_10MHZ_125KBIT);
   mcp2515_setCANINTE(0x03); // Inturrupt when a message is received
   mcp2515_setRXBnCTRL(0x60, 0x60); // Ignore filters, receive all messages
   mcp2515_setMode(MODE_LOOPBACK); // Loopback sends messages to itself for testing
+
+  Serial.print("Transmitted message:    ");
+  printCanMessage();
+  mcp2515_loadTX0(&message);
+  mcp2515_clearCANINTF(0xFF);
+  mcp2515_rtsTX0();
+
+  message = {0}; // resets the message to 0 to verify readRX0 works properly
+  Serial.print("Message before readRX0: ");
+  printCanMessage();
 }
 
 void loop() {
   // The INT pin will go low when there is an interrupt
-  if (!digitalRead(9)) {
-    Serial.println("Interrupt");
+  if (!digitalRead(INT)) {
+    Serial.print("Interrupt\nMessage after readRX0:  ");
+    mcp2515_readRX0(&message);
+    printCanMessage();
+    mcp2515_clearCANINTF(0xFF);
   }
 }
